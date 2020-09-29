bf7612d1616b67b540efe878e062ea5db327005b
jchable <yahicimosclick9193@gmail.com>
2020-09-29 00:42:45 -0500

    crud-api-nestjs-mongoose - Uso de variables de ambiente

--- crud-api-nestjs-mongoose/src/app.module.ts
+++ crud-api-nestjs-mongoose/src/app.module.ts
@@ -1,21 +1,17 @@
+// Modules
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { AppConfigModule } from './config/app/config.module';
+import { MongoDBConfigModule } from './config/db/mongo/config.module';
+
+// Controllers
 import { AppController } from './app.controller';
+
+// Services
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { ConfigModule } from '@nestjs/config';
-import { AppConfig } from '../config/app.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: AppConfig,
-    }),
-    MongooseModule.forRoot(
-      'mongodb+srv://testuser:4APWK5m^CuT8@guiltyspark.qsbai.mongodb.net/RoomieTest?retryWrites=true&w=majority',
-    ),
-    ProductsModule,
-  ],
+  imports: [AppConfigModule, MongoDBConfigModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
